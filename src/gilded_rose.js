4d94041d7feacd0fe58782e7a4ea412c9a11dcfe
Ryan Bass <theryanbass@gmail.com>
2022-04-19 11:35:04 -0700

    condense spacing

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,19 +10,17 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-
       if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
         this.items[i].quality = 80;
         continue;
       }
-
       if (this.items[i].name === 'Aged Brie') {
         this.items[i].quality = Math.min(50, this.items[i].quality + 1);
         continue;
       }
-
       if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert'){
         if (this.items[i].sellIn <= 0) {
           this.items[i].quality = 0;
@@ -31,18 +29,14 @@ class Shop {
         }
         continue;
       }
-
       if (this.items[i].name.includes('Conjured')) {
         (this.items[i].sellIn < 1) ? this.items[i].quality -= 4 : this.items[i].quality -= 2;
         this.items[i].quality = Math.max(0, this.items[i].quality);
         continue;
       }
-
       (this.items[i].sellIn < 1) ? this.items[i].quality -= 2 : this.items[i].quality -= 1;
       this.items[i].quality = Math.max(0, this.items[i].quality);
-
     }
-
     return this.items;
   }
 }
