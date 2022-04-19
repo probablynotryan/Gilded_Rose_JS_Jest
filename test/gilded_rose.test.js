3ed98d6b99751df5d94d7a2621d81bf230ff2b44
Ryan Bass <theryanbass@gmail.com>
2022-04-19 09:58:04 -0700

    add sulfuras condition with test

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -6,4 +6,10 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("aged brie");
   });
+
+  it("create an object named 'Sulfuras, Hand of Ragnaros' with a quality of 80", function() {
+    const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  });
 });
