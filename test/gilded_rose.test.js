45e6495f0312f3ad8709cef0d3b177b64e7fca9d
Ryan Bass <theryanbass@gmail.com>
2022-04-19 09:51:47 -0700

    add obj create greenlight test

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,9 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  it("create an object named 'aged brie'", function() {
+    const gildedRose = new Shop([new Item("aged brie", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("aged brie");
   });
 });
