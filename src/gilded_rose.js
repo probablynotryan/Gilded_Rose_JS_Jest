b5af9c2d4c88acdcc6db27edd7caf8f02c480682
Ryan Bass <theryanbass@gmail.com>
2022-04-19 10:21:38 -0700

    WIP: backstage passes conditional

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -20,10 +20,13 @@ class Shop {
         !(this.items[i].quality >= 50) && (this.items[i].quality += 1);
         continue;
       }
+      if ((this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') && (this.items[i].sellIn > 0)){
+        this.items[i].quality += 1 + (this.items[i].sellIn < 11) ? 2 : (this.items[i].sellIn < 6) && 3; 
+        continue;
+      }
 
 
-
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
             this.items[i].quality = this.items[i].quality - 1;
         }
@@ -46,13 +49,10 @@ class Shop {
       }
       this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
+
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
