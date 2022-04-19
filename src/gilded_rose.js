640de14a1a8f395b52c6be8895adf78c472a24f2
Ryan Bass <theryanbass@gmail.com>
2022-04-19 10:09:55 -0700

    add aged brie condition

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -16,15 +16,16 @@ class Shop {
         this.items[i].quality = 80;
         continue;
       }
-
+      if (this.items[i].name === 'Aged Brie') {
+        !(this.items[i].quality >= 50) && (this.items[i].quality += 1);
+        continue;
+      }
 
 
 
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
-          }
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -43,9 +44,7 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+      this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,10 +1,16 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("create an object named 'aged brie'", function() {
-    const gildedRose = new Shop([new Item("aged brie", 0, 0)]);
+  it("create an object named 'Aged Brie' that increases in quality by 1", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("aged brie");
+    expect(items[0].quality).toBe(1);
+  });
+
+  it("create an object named 'Aged Brie' that does not exceed a quality of 50", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
   });
 
   it("create an object named 'Sulfuras, Hand of Ragnaros' with a quality of 80", function() {
